27ade86058e32798f4bfeaa05289a7fb6d7cfa96
agusbrocani <aguss.brocani@gmail.com>
2026-03-24 18:50:58 -0300

    Ejercicio de datos estructurados

---
+++ ejercicioDatosEstructurados.py
@@ -0,0 +1,57 @@
+from IPython.display import Markdown
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+from anthropic import Anthropic
+
+client = Anthropic()
+model = "claude-sonnet-4-0"
+
+def add_user_message(message, text):
+    user_message = {"role": "user", "content": text}
+    message.append(user_message)
+
+def add_assistant_message(message, text):
+    assistant_message = {"role": "assistant", "content": text}
+    message.append(assistant_message)
+
+def chat(messages, system=None, temperature=0.0, stop_sequences=None):
+    params = {
+        "model": model,
+        "max_tokens": 1000,
+        "messages": messages,
+        "temperature": temperature
+    }
+
+    if system is not None:
+        params["system"] = system
+
+    if stop_sequences is not None:
+        params["stop_sequences"] = stop_sequences
+
+    response = client.messages.create(**params)
+    return response.content[0].text
+
+messages = []
+
+prompt = """
+Generate three different sample AWS CLI commands. Each should be very short.
+"""
+
+add_user_message(messages, prompt)
+
+add_assistant_message(messages, "Here are all three commands in a single block without any comments:\n```bash")
+
+text = chat(messages, stop_sequences=["```"])
+text = chat(messages)
+
+# print(text)
+# print(repr(text))
+
+clean = text.strip()
+
+if clean.endswith("```"):
+    clean = clean[:-4]
+
+print(clean)
